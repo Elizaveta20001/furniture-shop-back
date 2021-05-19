f91c931186e47b62fca020f54edef110d7c583ba
Elizaveta <kovel.liza@mail.ru>
2021-05-19 15:28:42 +0300

    Added get collection item.

--- helpers/templateHandler.ts
+++ helpers/templateHandler.ts
@@ -16,3 +16,16 @@ export const templateHandler = (collectionName: string, Model: any) => async (re
         response.status(500).send(error)
     }
 };
+
+export const templateCollectionItemHandler = (collectionName: string, Model: any) => async (request:Request, response:Response) =>{
+    const id = request.params.id;
+    try{
+        const query = {title: collectionName, 'items.id': id };
+        const collection = await Model.findOne(query,{ "items.$": id});
+        response.json(
+            ...collection.items
+        )
+    }catch (error) {
+        response.status(500).send(error);
+    }
+};
--- routes/collection.routes.ts
+++ routes/collection.routes.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { templateHandler } from '../helpers/templateHandler';
+import {templateCollectionItemHandler, templateHandler} from '../helpers/templateHandler';
 import Collection from '../models/Collection';
 
 
@@ -10,16 +10,32 @@ router.get(
     templateHandler('Armchairs', Collection)
 );
 router.get(
+    '/armchairs/:id',
+    templateCollectionItemHandler("Armchairs", Collection)
+);
+router.get(
     '/tables',
     templateHandler('Tables', Collection)
+);
+router.get(
+    '/tables/:id',
+    templateCollectionItemHandler('Tables', Collection)
 );
 router.get(
     '/sofas',
     templateHandler("Sofas", Collection)
 );
 router.get(
+    '/sofas/:id',
+    templateCollectionItemHandler("Sofas", Collection)
+);
+router.get(
     '/beds',
     templateHandler("Beds", Collection)
+);
+router.get(
+    '/beds/:id',
+    templateCollectionItemHandler("Beds", Collection)
 );
 
 
