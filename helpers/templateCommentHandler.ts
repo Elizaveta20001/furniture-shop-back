522e6cc109a1a9aab578c61a0745f290fb0e55ce
Elizaveta <kovel.liza@mail.ru>
2021-05-27 13:57:17 +0300

    Refactored comment post request.

--- helpers/templateCommentHandler.ts
+++ helpers/templateCommentHandler.ts
@@ -1,13 +1,13 @@
 import {Request, Response} from "express";
+import {Types} from "mongoose";
 
 import User from "../models/User";
 import CommentSchema from "../models/Comment";
-import {Types} from "mongoose";
 
 
-export const templateCommentHandler = (collectionName: string, Model: any) => async (request:Request, response:Response) =>{
+export const templateCommentHandler = (collectionName: string, Model: any) => async (request: Request, response: Response) => {
     const {text, createdAt} = request.body;
-    try{
+    try {
         const userData: any = await User.findOne({_id: request.body.user});
         const email = userData.email;
 
@@ -18,11 +18,10 @@ export const templateCommentHandler = (collectionName: string, Model: any) => as
             id: new Types.ObjectId()
         });
 
-        const query = {title: collectionName, 'items.id': request.params.id };
-        const collection: any = await Model.findOne(query,{ "items.$": request.params.id});
-        console.log(collection);
-        response.status(200).json("OK");
-    }catch (error){
+        const query = {title: collectionName, items: {$elemMatch: {id: request.params.id}}};
+        await Model.findOneAndUpdate(query, {$push: {'items.$.comments': newComment}}, {useFindAndModify: false});
+        response.status(200);
+    } catch (error) {
         response.status(500).json(error);
     }
 };
