68dd7f6bf94f51db589201907c0d45aee2ba3817
NikitaKliushun <samovarik_27@mail.ru>
2021-05-31 23:10:38 +0300

    switched SearchResults endpoint from POST to GET, modified search logic

--- routes/serachResult.routes.ts
+++ routes/serachResult.routes.ts
@@ -1,29 +1,30 @@
 import { Router, Request, Response } from 'express';
 import Collection from '../models/Collection';
 
-
 const router = Router();
 
 router.get(
     '/',
     async (req: Request, res: Response) => {
         try{
-            const {field, collectionName} = req.body;
+            const {field, collectionName} = req.query;
+            let searchCondition:any;
 
-            const resultCollection = field && collectionName ? await Collection.aggregate(
+            collectionName ?
+                searchCondition = {
+                    'items.title': { "$regex": field, "$options": "i" },
+                    'title' : collectionName
+                } : searchCondition = { 'items.title': { "$regex": field, "$options": "i" } }
+
+            const resultCollection = field ? await Collection.aggregate(
                 [
-                    {"$match": {
-                            'title': collectionName,
-                            'items.title': { "$regex": field, "$options": "i" },
-                    }},
+                    {"$match": searchCondition},
                     {"$unwind": "$items"},
                     {"$match": {'items.title': { "$regex": field, "$options": "i" }}},
                 ]) : [];
 
-            const result = resultCollection.map( (item) => item.items )
-
             res.json({
-                searchResult: result
+                searchResult: resultCollection.map( (item) => item.items )
             })
         } catch(e){
             res.status(500).json({message: 'something goes wrong'});
