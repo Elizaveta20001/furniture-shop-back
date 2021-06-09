f7ca2f440b678f0f2ce9d30d25e1ee0c8db17cbc
Elizaveta <kovel.liza@mail.ru>
2021-06-09 12:50:21 +0300

    Removed console log in getItemData.

--- helpers/getItemData.ts
+++ helpers/getItemData.ts
@@ -2,7 +2,6 @@ import Collection from "../models/Collection";
 
 export const getItemData = async (id: string) =>{
     const itemData: any = await Collection.findOne({'items.id': id}, {'items.$': id, 'title': id});
-    console.log(itemData);
     return itemData;
 
 }
